76dd124a67cf31ab55cd5686bb9ce87db35eb61b
KC29072001 <chandelkartikey2001@gmail.com>
2025-04-19 18:19:01 +0530

    changes_readme-and-godotenv

--- cmd/main.go
+++ cmd/main.go
@@ -1,3 +1,58 @@
+// package main
+
+// import (
+// 	"context"
+// 	"log"
+// 	"os"
+// 	"os/signal"
+// 	"syscall"
+// 	"time"
+
+// 	"caller/internal/config"
+// 	"caller/internal/server"
+// )
+
+// func main() {
+// 	if err := run(); err != nil {
+// 		log.Printf("error: %v\n", err)
+// 		os.Exit(1)
+// 	}
+// }
+
+// func run() error {
+// 	ctx, cancel := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+// 	defer cancel()
+
+// 	cfg, err := config.Load()
+// 	if err != nil {
+// 		return err
+// 	}
+
+// 	srv, err := server.New(cfg)
+// 	if err != nil {
+// 		return err
+// 	}
+
+// 	go func() {
+// 		if err := srv.Start(); err != nil {
+// 			log.Printf("server error: %v\n", err)
+// 		}
+// 	}()
+
+// 	<-ctx.Done()
+
+// 	shutdownCtx, shutdownCancel := context.WithTimeout(context.Background(), 10*time.Second)
+// 	defer shutdownCancel()
+
+// 	if err := srv.Shutdown(shutdownCtx); err != nil {
+// 		return err
+// 	}
+
+// 	return nil
+// }
+
+
+
 package main
 
 import (
@@ -7,6 +62,8 @@ import (
 	"os/signal"
 	"syscall"
 	"time"
+
+	"github.com/joho/godotenv" // Import the godotenv package
 
 	"caller/internal/config"
 	"caller/internal/server"
@@ -20,6 +77,16 @@ func main() {
 }
 
 func run() error {
+	// Load .env file
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("Error loading .env file:", err)
+		// You might choose to return the error here, or handle it in some other way
+		// For example, you could proceed with default configuration values.
+		// For this example, we'll return the error to stop execution.
+		return err 
+	}
+
 	ctx, cancel := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
 	defer cancel()
 
